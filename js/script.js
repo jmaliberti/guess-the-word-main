681bc069da6f659392e19eefc1599342b3385501
Jessica Aliberti <jmaliberti@gmail.com>
2024-07-18 13:37:55 -0700

    Created initial global variables for project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const textInput = document.querySelector("#letter");
+const wordMaking = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingSpan = document.querySelector(".remaining span");
+const messageParagraph = document.querySelector(".message");
+const playButton = document.querySelector(".play-again");
+const word = "magnolia";
